540ca65819b98d4c56ca602f45ebfc6ae7922c18
Pierre Belin <p.belin.ext@stid.com>
2025-07-03 18:19:34 +0200

    x

--- LaurianeCsharp.Tests/UnitTest1.cs
+++ LaurianeCsharp.Tests/UnitTest1.cs
@@ -1,54 +1,95 @@
 ﻿using LaurianeCsharp.Lib;
 
-namespace LaurianeCsharp.Tests
+namespace LaurianeCsharp.Tests;
+
+public class UnitTest1
 {
-    public class UnitTest1
+    [Fact]
+    public void TestMaMethode1()
     {
-        [Fact]
-        public void TestMaMethode1()
+        string a = "test";
+        string testt = "test" + "test";
+        string testtt = $"test{a}"; // testtest
+        int test2 = 1;
+        bool test3 = true;
+        byte test4 = 0x0F;
+        char test10 = 'a';
+
+        float test5 = 0.01f;
+        double test6 = 0.01;
+        decimal test7 = 0.01m;
+
+        short test8 = 1;
+        long test9 = 1;
+            
+        DateTime test11 = DateTime.UtcNow;
+        DateTime test12 = new DateTime(2025,01,01);
+
+        var aaaaa = new MyDiscoveringClass();
+        var bbb = new MyDiscoveringClass("XXXX");
+        // aaaaa.MyProperty = "test";
+            
+        MyDiscoveringClass.MyMethodeStatic();
+        // bbb.MyMethodeStatic();
+        var zdqd= MyDiscoveringClass.MY_CONSTANTE;
+            
+        var myStaticConstructor = MyStaticConstructorClass.Create("test");
+        //var myStaticConstructor = new MyStaticConstructorClass("test");
+
+        var myEnum = MyEnum.Val1;
+        var myFlag = MyFlag.Val3 | MyFlag.Val2;
+            
+        var myList = new List<string>();
+        List<string> myList2 = [];
+
+        foreach (var xx in myList2)
         {
-            string a = "test";
-            string testt = "test" + "test";
-            string testtt = $"test{a}"; // testtest
-            int test2 = 1;
-            bool test3 = true;
-            byte test4 = 0x0F;
-            char test10 = 'a';
-
-            float test5 = 0.01f;
-            double test6 = 0.01;
-            decimal test7 = 0.01m;
-
-            short test8 = 1;
-            long test9 = 1;
-            
-            DateTime test11 = DateTime.UtcNow;
-            DateTime test12 = new DateTime(2025,01,01);
-
-            var aaaaa = new MyDiscoveringClass();
-            var bbb = new MyDiscoveringClass("XXXX");
-            // aaaaa.MyProperty = "test";
-            
-            MyDiscoveringClass.MyMethodeStatic();
-            // bbb.MyMethodeStatic();
-            var zdqd= MyDiscoveringClass.MY_CONSTANTE;
-            
-            var myStaticConstructor = MyStaticConstructorClass.Create("test");
-            //var myStaticConstructor = new MyStaticConstructorClass("test");
-
-            var myEnum = MyEnum.Val1;
-            var myFlag = MyFlag.Val3 | MyFlag.Val2;
-            
-            var myList = new List<string>();
-            List<string> myList2 = [];
-
-            foreach (var xx in myList2)
-            {
-                // xx
-            }
+            // xx
         }
     }
+        
+    [Fact]
+    public void TestMaMethode2()
+    {
+        var myClass = new MyClass();
+        var myAttribute = myClass.GetMyAttribute().Trim();
+
+        var aa = "aa";
+        aa.GetMyAttributeTrim();
+        // var a = MyClassExtensions.GetMyAttributeTrim(myClass);
+        var a = myClass.GetMyAttributeTrim();
+    }
 }
+
+public class MyClass
+{
+    private string _myAttribute;
+    private string _myAttribute2;
+    
+    public string GetMyAttribute()
+    {
+        return _myAttribute;
+    }
+}
+
+public static class MyClassExtensions
+{
+    public static string GetMyAttributeTrim(this string myClass)
+    {
+        return myClass.Trim();
+    }
+    
+    public static string GetMyAttributeTrim(this MyClass myClass)
+    {
+        return myClass.GetMyAttribute().Trim();
+    }
+    
+    // public static string GetMyAttributeTrim(MyClass myClass)
+    // {
+    //     return myClass.GetMyAttribute().Trim();
+    // }
+}
+
 
 // - Génériques
 //     - Interfaces - multiple
---
+++ LaurianeCsharp.Tests/UnitTest2.cs
@@ -0,0 +1,79 @@
+﻿using System.Text.Json;
+
+namespace LaurianeCsharp.Tests;
+
+public class UnitTest2
+{
+    [Fact]
+    public void TestMaMethode1()
+    {
+        var list = new List<ICard>
+        {
+            new VCard(),
+            new VConf()
+        };
+
+        foreach (ICard card in list)
+        {
+            card.MyMethod();
+            
+            if (card.GetType() == typeof(VCard))
+            {
+                ((VCard)card).AA();
+            }
+            // EGAUX
+            if (card is VCard vcard)
+                vcard.AA();
+            
+            else if (card is VConf vconf)
+                vconf.BB();
+            
+            card.MyGeneric();
+        }
+
+
+        var aaaa = "xxxx";
+        JsonSerializer.Deserialize<MyClass>(aaaa);
+
+    }
+    
+}
+
+public static class MyClassExtensions2
+{
+    public static void MyGeneric<T>(this T myClass) where T : ICard
+    {
+        //
+        myClass.MyMethod();
+        //
+    }
+}
+
+public class VCard : ICard
+{
+    public void MyMethod()
+    {
+        throw new NotImplementedException();
+    }
+    public void AA()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class VConf : ICard
+{
+    public void MyMethod()
+    {
+        throw new NotImplementedException();
+    }
+    public void BB()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public interface ICard
+{
+    void MyMethod();
+}
